331da1c5348e369b0aff19feec24b602210b83b9
Nathan Wallace <nathan@nosuchthingastwo.com>
2017-05-04 22:20:29 -0400

    add --local option to install_host_app.sh
    Some users may be concerned with executing a script on their machine that
    downloads files from the internet. This option allows users to opt out
    of that behavior in favor of using files locally on disk instead.

--- src/host/install_host_app.sh
+++ src/host/install_host_app.sh
@@ -29,7 +29,7 @@ else
     TARGET_DIR_CHROME="/etc/opt/chrome/native-messaging-hosts"
     TARGET_DIR_CHROMIUM="/etc/chromium/native-messaging-hosts"
     TARGET_DIR_FIREFOX="/usr/lib/mozilla/native-messaging-hosts"
-    TARGET_DIR_VIVALDI="/etc/chromium/native-messaging-hosts"
+    TARGET_DIR_VIVALDI="/etc/vivaldi/native-messaging-hosts"
   else
     TARGET_DIR_CHROME="$HOME/.config/google-chrome/NativeMessagingHosts"
     TARGET_DIR_CHROMIUM="$HOME/.config/chromium/NativeMessagingHosts"
@@ -38,32 +38,55 @@ else
   fi
 fi
 
-case $1 in
---chrome)
-  BROWSER_NAME="Chrome"
-  TARGET_DIR="$TARGET_DIR_CHROME"
-  ;;
---chromium)
-  BROWSER_NAME="Chromium"
-  TARGET_DIR="$TARGET_DIR_CHROMIUM"
-  ;;
---firefox)
-  BROWSER_NAME="Firefox"
-  TARGET_DIR="$TARGET_DIR_FIREFOX"
-  ;;
---opera)
-  BROWSER_NAME="Opera"
-  TARGET_DIR="$TARGET_DIR_VIVALDI"
-  ;;
---vivaldi)
-  BROWSER_NAME="Vivaldi"
-  TARGET_DIR="$TARGET_DIR_VIVALDI"
-  ;;
-*)
-    echo "Usage: $0 [--chrome|--chromium|--firefox|--opera|--vivaldi]"
-    exit 1
-    ;;
-esac
+function usage {
+  echo "Usage: $0 [OPTION] [chrome|chromium|firefox|opera|vivaldi]
+
+  Options:
+    -l, --local    Install files from disk instead of downloading them
+    -h, --help     Show this message"
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    chrome)
+      BROWSER_NAME="Chrome"
+      TARGET_DIR="$TARGET_DIR_CHROME"
+      ;;
+    chromium)
+      BROWSER_NAME="Chromium"
+      TARGET_DIR="$TARGET_DIR_CHROMIUM"
+      ;;
+    firefox)
+      BROWSER_NAME="Firefox"
+      TARGET_DIR="$TARGET_DIR_FIREFOX"
+      ;;
+    opera)
+      BROWSER_NAME="Opera"
+      TARGET_DIR="$TARGET_DIR_VIVALDI"
+      ;;
+    vivaldi)
+      BROWSER_NAME="Vivaldi"
+      TARGET_DIR="$TARGET_DIR_VIVALDI"
+      ;;
+    -l|--local)
+      USE_LOCAL_FILES=true
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [ -z "$TARGET_DIR" ]; then
+  usage
+  exit 1
+fi
 
 HOST_FILE_PATH="$TARGET_DIR/$APP_NAME.py"
 MANIFEST_FILE_PATH="$TARGET_DIR/$APP_NAME.json"
@@ -74,9 +97,15 @@ echo "Installing $BROWSER_NAME host config"
 # Create config dir if not existing
 mkdir -p "$TARGET_DIR"
 
-# Download native host script and manifest
-curl -s "$HOST_URL"     > "$HOST_FILE_PATH"
-curl -s "$MANIFEST_URL" > "$MANIFEST_FILE_PATH"
+if [ "$USE_LOCAL_FILES" = true ]; then
+  DIR="$( cd "$( dirname "$0" )" && pwd )"
+  cp "$DIR/passff.py"   "$HOST_FILE_PATH"
+  cp "$DIR/passff.json" "$MANIFEST_FILE_PATH"
+else
+  # Download native host script and manifest
+  curl -sSL "$HOST_URL"     > "$HOST_FILE_PATH"
+  curl -sSL "$MANIFEST_URL" > "$MANIFEST_FILE_PATH"
+fi
 
 # Replace path to host
 sed -i -e "s/PLACEHOLDER/$ESCAPED_HOST_FILE_PATH/" "$MANIFEST_FILE_PATH"
