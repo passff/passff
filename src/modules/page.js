09f1dfc72148ee22d5e79b762743973f206cfb2c
5bentz <10729484+5bentz@users.noreply.github.com>
2018-05-31 00:42:02 +0200

    Fix invisible input menu's icon
    Input fields with the style '-moz-appearance: window' hide the PassFF icon.

--- src/modules/page.js
+++ src/modules/page.js
@@ -241,6 +241,8 @@ PassFF.Page = (function () {
     if (typeof input.passff_injected !== "undefined") return;
     log.debug("Inject icon", input.id || input.name);
     input.passff_injected = true;
+    input.style["-moz-appearance"] = "none";
+    input.style["-webkit-appearance"] = "none";
     input.style.backgroundRepeat = "no-repeat";
     input.style.backgroundAttachment = "scroll";
     input.style.backgroundSize = "16px 16px";
